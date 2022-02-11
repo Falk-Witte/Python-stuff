352902941cf73436154fb5d867c599238c0947af
Falk-Witte <falk.witte15@gmail.com>
2022-02-11 10:18:26 +0100

    made some changes to encryption-machine.py

--- encryption-machine.py
+++ encryption-machine.py
@@ -1,4 +1,4 @@
-#!bin/python3
+#!/usr/bin/python3
 import time
 from database import pass_data
 import hashlib
@@ -14,16 +14,19 @@ def encryption_machine():
 
       #message
   f = input("Enter to encode word=> ")
+  setback= int(input("Enter amount of setback for Rot13=> "))
 
-  rot13 = lambda x: "".join([alph[(alph.find(c) + 13) %len(f)] for c in x])
+  rot13 = lambda x: "".join([alph[(alph.find(c) + setback) %len(f)] for c in x])
       
   i = rot13(f)
+
+  print("Rot13 output=>", i)
 
   a = pyfiglet.figlet_format(i, font='hex')
 
   print("Encoded text=> ", a)
 
-  print("Number of letters moved in rot13:",len(f))
+  #print("Number of letters moved in rot13:",len(f))
 
 
 #password authentication
@@ -36,12 +39,15 @@ ha = h.hexdigest()
 #checking if two values are the same
 
 #initialization loop 
-if pass_data() in str(ha):
-  time.sleep(1)
-  print(encryption_machine())
-else:
-  time.sleep(1)
-  print("Wrong password")
-  time.sleep(1)
-  print("Cancelling Process...")
-  time.sleep(1)
+try:
+  if pass_data() in str(ha):
+    time.sleep(1)
+    encryption_machine()
+  else:
+    time.sleep(1)
+    print("Wrong password")
+    time.sleep(1)
+    print("Cancelling Process...")
+    time.sleep(1)
+except:
+  print("There has been an error")
