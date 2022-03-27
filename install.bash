d27087c2157068601bfad552a0f2c71462cdb0e4
Falk-Witte <falk.witte15@gmail.com>
2022-03-27 09:25:31 +0200

    changed script

--- install.bash
+++ install.bash
@@ -14,8 +14,9 @@ git clone https://github.com/Falk-Witte/Python-stuff
 
 #install colorama
 colorama=~/.local/lib/python3.10/site-packages/colorama/
+colorama2=/usr/lib/python3.10/site-packages/colorama/
 
-if [[ -d $colorama ]]
+if [[ -d $colorama ]] || [[ -d $colorama2 ]]
 then
     echo "colorama is installed"
 else
@@ -24,8 +25,9 @@ fi
 
 #install pyfiglet
 pyfiglet=/usr/lib/python3.10/site-packages/pyfiglet/
+pyfiglet2=~/.local/lib/python3.10/site-packages/pyfiglet/
 
-if [[ -d $pyfiglet ]]
+if [[ -d $pyfiglet ]] || [[ -d $pyfiglet2 ]]
 then
     echo "pyfiglet is installed"
 else
@@ -34,8 +36,9 @@ fi
 
 #install py-sudoku
 sudoku=/usr/lib/python3.10/site-packages/py_sudoku-1.0.2.dist-info/
+sudoku2=~/.local/lib/python3.10/site-packages/py_sudoku-1.0.2.dist-info/
 
-if [[ -d $sudoku ]]
+if [[ -d $sudoku ]] || [[ -d $sudoku2 ]]
 then
     echo "py-sudoku is installed"
 else
